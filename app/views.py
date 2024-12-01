b89bb85062811dde3ad1ae957da88d1356e9fe4c
Seno Wijayanto <senowijayanto@gmail.com>
2024-12-01 21:22:31 +0700

    add loading on searching button

--- app/views.py
+++ app/views.py
@@ -1,3 +1,4 @@
+import time
 from typing import Any
 
 from django.contrib import messages
@@ -20,6 +21,7 @@ class ArticleListView(LoginRequiredMixin, ListView):
     paginate_by = 5
 
     def get_queryset(self) -> QuerySet[Any]:
+        time.sleep(2)
         search = self.request.GET.get('search')
         queryset = super().get_queryset().filter(creator=self.request.user)
         if search:
